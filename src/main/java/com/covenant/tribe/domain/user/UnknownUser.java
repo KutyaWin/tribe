cdb0b5c9bc47427e18fb7c294712f3273da6bb81
MrShtein <mr.shtein@outlook.com>
2023-04-09 15:46:17 +0300

    [feat] Added entity for unknown_user and unknown_user_interests

---
+++ src/main/java/com/covenant/tribe/domain/user/UnknownUser.java
@@ -0,0 +1,54 @@
+package com.covenant.tribe.domain.user;
+
+
+import com.covenant.tribe.domain.event.EventType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "unknown_users")
+public class UnknownUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "bluetooth_id", nullable = false, unique = true, length = 100)
+    String bluetoothId;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "unknown_user_interests",
+            joinColumns = {@JoinColumn(name = "unknown_user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "event_type_id")}
+    )
+    @ToString.Exclude
+    List<EventType> userInterests = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        UnknownUser unknownUser = (UnknownUser) o;
+        return this.id != null && this.id.equals(unknownUser.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getClass().hashCode();
+    }
+}
