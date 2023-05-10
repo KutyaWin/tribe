27057d71c56513ce2d180f7551a3bb626012f5ad
MrShtein <mr.shtein@outlook.com>
2023-05-10 19:06:13 +0300

    [TRB-118] Added endpoint for getting events by organizer id

--- src/main/java/com/covenant/tribe/repository/EventRepository.java
+++ src/main/java/com/covenant/tribe/repository/EventRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByEventNameAndStartTimeAndOrganizerId(String eventName, OffsetDateTime startTime, Long organizerId);
+    List<Event> findAllByOrganizerId(Long organizerId);
 }
