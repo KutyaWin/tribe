2ae4c593355d598e4ab1a33827c489f1601e5e0e
MrShtein <mr.shtein@outlook.com>
2023-05-16 20:10:01 +0300

    [TRB-139] Added enpoint for getting all subscribers

--- src/main/java/com/covenant/tribe/controller/UserController.java
+++ src/main/java/com/covenant/tribe/controller/UserController.java
@@ -2,6 +2,7 @@ package com.covenant.tribe.controller;
 
 import com.covenant.tribe.dto.event.EventInFavoriteDTO;
 import com.covenant.tribe.dto.user.UserFavoriteEventDTO;
+import com.covenant.tribe.dto.user.UserSubscriberDto;
 import com.covenant.tribe.dto.user.UserToSendInvitationDTO;
 import com.covenant.tribe.service.UserService;
 import com.covenant.tribe.util.mapper.EventMapper;
@@ -19,6 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,7 +51,7 @@ public class UserController {
     public ResponseEntity<?> findUserByUsernameForSendInvite(
             @PathVariable(value = "username") String partialUsername,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "10") Integer size
+            @RequestParam(value = "size", defaultValue = "100") Integer size
     ) {
         log.info("[CONTROLLER] start endpoint findUserByUsernameForSendInvite with param: {}", partialUsername);
 
@@ -59,6 +61,37 @@ public class UserController {
                 userService.findUsersByContainsStringInUsernameForSendInvite(partialUsername, pageable);
 
         log.info("[CONTROLLER] end endpoint findUserByUsernameForSendInvite with response: {}", responseUser);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseUser);
+    }
+
+    @Operation(
+            tags = "User",
+            description = "Screen подписчики. Get all user's subscribers.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(
+                                            schema = @Schema(implementation = UserSubscriberDto.class))))},
+            security = @SecurityRequirement(name = "BearerJWT")
+    )
+    @PreAuthorize("#userId.equals(authentication.getName())")
+    @GetMapping("/subscriber/partial/{subscriber_username}/{user_id}")
+    public ResponseEntity<?> findAllSubscribersByUsername(
+            @PathVariable(value = "subscriber_username") String subscriberUsername,
+            @PathVariable(value = "user_id") String userId,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "100") Integer size
+    ) {
+        log.info("[CONTROLLER] start endpoint findAllSubscribersByUsername with param: {}", subscriberUsername);
+
+        Pageable pageable = Pageable.ofSize(size).withPage(page);
+        Page<UserSubscriberDto> responseUser = userService.findAllSubscribersByUsername(subscriberUsername, Long.parseLong(userId), pageable);
+
+        log.info("[CONTROLLER] end endpoint findAllSubscribersByUsername with response: {}", responseUser);
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(responseUser);
