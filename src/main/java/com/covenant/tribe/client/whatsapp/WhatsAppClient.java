8dc087aa78f17a07696eaae8a7dd15a52c5e5a35
MrShtein <mr.shtein@outlook.com>
2023-06-09 16:37:08 +0300

    [TRB-246] Added capabilities to send messages by whatsapp api (After setting up Whatsapp business, needs to be tested.)

--- src/main/java/com/covenant/tribe/client/whatsapp/WhatsAppClient.java
+++ src/main/java/com/covenant/tribe/client/whatsapp/WhatsAppClient.java
@@ -1,13 +1,12 @@
 package com.covenant.tribe.client.whatsapp;
 
 import com.covenant.tribe.client.whatsapp.dto.WhatsAppVerificationMsgDto;
-import com.covenant.tribe.exeption.client.WhatsAppClientFallbackFactory;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "whatsapp-client", fallbackFactory = WhatsAppClientFallbackFactory.class)
+@FeignClient(name = "whatsapp-client")
 public interface WhatsAppClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "/{api-version}/{phone-id}/messages")
