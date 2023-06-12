865d0072821281c6f0334613f5b00da6309a426a
Mr.Shtein <mr.shtein@outlook.com>
2023-06-12 14:39:26 +0300

    [TRB-260] Added endpoint for confirming email changing

--- src/main/java/com/covenant/tribe/controller/UserController.java
+++ src/main/java/com/covenant/tribe/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.covenant.tribe.controller;
 
 import com.covenant.tribe.dto.ImageDto;
+import com.covenant.tribe.dto.auth.EmailConfirmCodeDto;
 import com.covenant.tribe.dto.storage.TempFileDTO;
 import com.covenant.tribe.dto.user.*;
 import com.covenant.tribe.service.PhotoStorageService;
@@ -285,13 +286,39 @@ public class UserController {
     @PreAuthorize("#userEmailDto.getUserId().equals(authentication.getName())")
     @PostMapping("/email/change/code")
     public ResponseEntity<?> sendEmailConfirmationCode(
-        @RequestBody UserEmailDto userEmailDto
+        @RequestBody @Valid UserEmailDto userEmailDto
     ) {
         log.info("[CONTROLLER] start endpoint sendEmailConfirmationCode with param: {}", userEmailDto);
 
         userService.sendConfirmationCodeToEmail(userEmailDto);
 
         log.info("[CONTROLLER] end endpoint sendEmailConfirmationCode");
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
+    }
+
+    @Operation(
+            description = "Категория: Профиль/ADMIN/USER/FOLLOWERS/MESSAGES/. Экран: Пока нет." +
+                    " Действие: Подтверждение кода при изменении email пользователя.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200"
+                    )
+            },
+            security = @SecurityRequirement(name = "BearerJWT")
+    )
+    @PatchMapping("/email/change/confirm")
+    public ResponseEntity<?> confirmEmailChange(
+            @RequestBody @Valid EmailChangeDto emailChangeDto
+
+    ) {
+        log.info("[CONTROLLER] start endpoint confirmEmailChange with param: {}", emailChangeDto);
+
+        userService.confirmEmailChange(emailChangeDto);
+
+        log.info("[CONTROLLER] end endpoint confirmEmailChange");
 
         return ResponseEntity
                 .status(HttpStatus.OK)
--- src/main/java/com/covenant/tribe/exeption/auth/ResetCodeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.covenant.tribe.exeption.auth;
-
-public class ResetCodeNotFoundException extends RuntimeException {
-    public ResetCodeNotFoundException(String message) {
-        super(message);
-    }
-}
