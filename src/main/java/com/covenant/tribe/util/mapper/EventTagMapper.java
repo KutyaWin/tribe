12da0ff654e003f77bfad3cbcfaa51bf16cf79cb
Mr.Shtein <mr.shtein@outlook.com>
2023-06-15 18:08:01 +0300

    [wip]

--- src/main/java/com/covenant/tribe/util/mapper/EventTagMapper.java
+++ src/main/java/com/covenant/tribe/util/mapper/EventTagMapper.java
@@ -4,6 +4,9 @@ package com.covenant.tribe.util.mapper;
 import com.covenant.tribe.domain.Tag;
 import com.covenant.tribe.dto.event.EventTagDTO;
 
+import java.util.List;
+
 public interface EventTagMapper {
          EventTagDTO mapEventTagToEventTagDTO(Tag eventTag);
+         List<EventTagDTO> mapEventTagListToEventTagDtoList(List<Tag> eventTags);
 }
