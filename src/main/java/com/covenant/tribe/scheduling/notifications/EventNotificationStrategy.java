d424de71fb739ce1367898b6adf9314917900589
Mr.Shtein <mr.shtein@outlook.com>
2023-06-26 16:20:47 +0300

    [TRB-264] Added participants to Notification

--- src/main/java/com/covenant/tribe/scheduling/notifications/EventNotificationStrategy.java
+++ src/main/java/com/covenant/tribe/scheduling/notifications/EventNotificationStrategy.java
@@ -5,34 +5,40 @@ import com.covenant.tribe.domain.user.User;
 import com.covenant.tribe.scheduling.model.BroadcastEntity;
 import com.covenant.tribe.scheduling.model.Notification;
 import com.covenant.tribe.service.EventService;
+import com.covenant.tribe.service.UserRelationsWithEventService;
 import com.covenant.tribe.service.UserService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 public class EventNotificationStrategy implements NotificationStrategy {
 
-    private final UserService userService;
-
-    private final EventService eventService;
+    UserService userService;
+    EventService eventService;
+    UserRelationsWithEventService userRelationsWithEventService;
 
     @Override
-    public List<Notification> getNotifications(BroadcastEntity entity) {
+    public List<Notification> getNotifications(BroadcastEntity broadcast) {
 
-
-        List<User> all = userService.findAll();
+        List<User> eventParticipants = userRelationsWithEventService
+                .findParticipantsByEventId(broadcast.getSubjectId());
         List<Notification> notifications = new ArrayList<>();
-        Event eventById = eventService.getEventById(entity.getSubjectId());
+        Event eventById = eventService.getEventById(broadcast.getSubjectId());
         String eventName = eventById.getEventName();
         for (int i = 0; i < 2; i++) {
-            User user = all.get(i);
+            User user = eventParticipants.get(i);
             Notification notification =
                     new Notification("notification for event name: " +
-                            eventName, user, entity);
+                            eventName, user, broadcast);
             notifications.add(notification);
         }
         return notifications;
