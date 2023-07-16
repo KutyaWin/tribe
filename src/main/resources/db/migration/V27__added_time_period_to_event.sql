26960d630dfbb4c4039398f0d61abdaf1e2de80f
BazileusBazileon <fjernprod@gmail.com>
2023-07-16 12:09:17 +0300

    - renamed migration

--- src/main/resources/db/migration/V27__added_time_period_to_event.sql
+++ src/main/resources/db/migration/V27__added_time_period_to_event.sql
@@ -1,4 +1,4 @@
-create table parts_of_day
+create table if not exists parts_of_day
 (
     id          BIGSERIAL  NOT NULL PRIMARY KEY,
     part_of_day int unique not null,
@@ -10,7 +10,7 @@ values (0, 'MORNING'),
        (1, 'AFTERNOON'),
        (2, 'EVENING'),
        (3, 'NIGHT');
-create table event_part_of_day
+create table if not exists event_part_of_day
 (
     event_id       BIGINT NOT NULL references events (id) ON DELETE CASCADE ON UPDATE CASCADE,
     part_of_day_id BIGINT NOT NULL references parts_of_day (id) ON DELETE CASCADE ON UPDATE CASCADE,
