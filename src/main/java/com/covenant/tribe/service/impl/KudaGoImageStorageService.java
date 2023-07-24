32092355cc889820a68ab273728da1c01d073ca7
Mr.Shtein <mr.shtein@outlook.com>
2023-07-24 19:01:21 +0300

    [wip]

--- src/main/java/com/covenant/tribe/service/impl/KudaGoImageStorageService.java
+++ src/main/java/com/covenant/tribe/service/impl/KudaGoImageStorageService.java
@@ -1,5 +1,7 @@
 package com.covenant.tribe.service.impl;
 
+import com.covenant.tribe.client.kudago.dto.KudagoEventDto;
+import com.covenant.tribe.client.kudago.dto.KudagoImageDto;
 import com.covenant.tribe.dto.ImageDto;
 import com.covenant.tribe.repository.FileStorageRepository;
 import com.covenant.tribe.service.ExternalImageStorageService;
@@ -7,7 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -16,7 +20,19 @@ public class KudaGoImageStorageService implements ExternalImageStorageService {
 
     FileStorageRepository fileStorageRepository;
     @Override
-    public void saveImage(List<ImageDto> images) {
+    public List<String> saveExternalImages(List<KudagoEventDto> events) {
+        Map<Long, List<String>> eventImages = new HashMap<>();
+        for(KudagoEventDto event : events) {
+            List<ImageDto> imageUrls = event.getImages().stream()
+                    .map(image -> {
+                        return ImageDto.builder()
+                                .
+                    })
+                    .toList();
+            List<String> imagePaths = fileStorageRepository.saveExternalEventImages(imageUrls);
+        }
 
+
+        return fileStorageRepository.saveExternalEventImages(events);
     }
 }
