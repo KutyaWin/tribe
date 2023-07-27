985c040be16b5b17e316f38d24135a0f67b9b942
Mr.Shtein <mr.shtein@outlook.com>
2023-07-27 17:26:31 +0300

    [wip]

--- src/main/java/com/covenant/tribe/service/ExternalEventService.java
+++ src/main/java/com/covenant/tribe/service/ExternalEventService.java
@@ -1,5 +1,6 @@
 package com.covenant.tribe.service;
 
+import com.covenant.tribe.client.dadata.dto.ReverseGeocodingData;
 import com.covenant.tribe.client.kudago.dto.KudagoEventDto;
 
 import java.util.List;
@@ -9,6 +10,13 @@ public interface ExternalEventService {
     List<KudagoEventDto> prepareEventsForCreating(
             Map<Long, KudagoEventDto> kudaGoEvents,
             int daysQuantityToFirstPublication
+    );
+
+    void saveNewExternalEvents(
+            List<KudagoEventDto> kudaGoEvents,
+            Map<Long, ReverseGeocodingData> reverseGeocodingData,
+            Map<Long, List<String>> imageFileNames,
+            Map<Long, List<Long>> eventTagIds
     );
 
 }
