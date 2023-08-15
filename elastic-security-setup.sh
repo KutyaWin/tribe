88eacfaad7f90e728e149613315046103c1df152
BazileusBazileon <fjernprod@gmail.com>
2023-08-15 20:17:53 +0300

    - added security to kibana

---
+++ elastic-security-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ELASTIC_PASSWORD=$(grep ELASTIC_PASSWORD .env | cut -d "=" -f2)
+
+docker-compose exec tribe_elasticsearch sh -c \
+"cd /usr/share/elasticsearch/bin && printf '%s\n' y $ELASTIC_PASSWORD $ELASTIC_PASSWORD |./elasticsearch-reset-password -u kibana_system -i"
