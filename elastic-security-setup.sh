38b7e4f51d7a56b1a015cd96c0cfc7005803400e
MrShtein <mr.shtein@outlook.com>
2023-09-22 19:56:04 +0300

    [feat] Adding a password for kibana, done without a script, with the help of an additional service setup.

--- elastic-security-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ELASTIC_PASSWORD=$(grep ELASTIC_PASSWORD .env | cut -d "=" -f2)
-
-docker compose exec dev_elasticsearch sh -c \
-"cd /usr/share/elasticsearch/bin && printf '%s\n' y $ELASTIC_PASSWORD $ELASTIC_PASSWORD |./elasticsearch-reset-password -u kibana_system -i"
